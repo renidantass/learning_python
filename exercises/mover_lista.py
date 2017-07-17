277cd9f6480ad3621af50517acf7007f94b335b6
Reni Dantas <reni.alkimim@gmail.com>
2017-07-16 21:47:30 -0300

    Move items in right side

---
+++ exercises/mover_lista.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+"""
+    Exercícios de: https://github.com/pythoneiros/Exercicios#lista-de-exercícios-já-propostos
+"""
+
+
+
+def move_list(args, times):
+    for _ in range(times):
+        tmp = args.pop()
+        args.insert(0, tmp)
+    return args
+
+print move_list([1,2,3], 4) # Example

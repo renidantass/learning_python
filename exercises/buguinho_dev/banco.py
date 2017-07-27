3e3630e0a911645640d8766e14366f8f5357e52d
Reni A. Dantas <reni.alkimim@gmail.com>
2017-07-26 21:14:21 -0300

    Finally???!

--- exercises/buguinho_dev/banco.py
+++ exercises/buguinho_dev/banco.py
@@ -2,6 +2,7 @@
 """
     Módulo por parte do cliente para o programa do banco Bugginho
 """
+from time import strftime
 import sqlite3
 
 
@@ -31,7 +32,6 @@ class Client(object):
             self.__cur.execute('''CREATE TABLE transacoes (id INTEGER PRIMARY KEY,
                                                             client_id INTEGER NOT NULL,
                                                             destination_id INTEGER NOT NULL,
-                                                            operation_id INTEGER NOT NULL,
                                                             value INTEGER NOT NULL, 
                                                             data TEXT)''')
             self.__conn.commit()
@@ -46,14 +46,18 @@ class Client(object):
         try:
             sql = '''SELECT * FROM contas WHERE agencia = {} AND conta = {}'''.format(self.agencia, self.conta)
             r =  self.__cur.execute(sql).fetchall()
-            return r[0] if r else 0
+            if len(r) == 0:
+                print 'Usuário não cadastrado'
+            else:
+                return r[0]
         except Exception as e:
             raise Exception(e)
 
     def __enter__(self):
         """ Método para usá-lo com with """
+        self.__logar()
         try:
-            print '\n{} entrou'.format(self.__user[1])
+            print '\nBanco informa: {} logou-se no sistema'.format(self.__user[1])
         except Exception as e:
             pass
         finally:
@@ -62,6 +66,14 @@ class Client(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         """ Mesma coisa do acima :p """
         self.__conn.close() # Certificando-se de fechar a conexão do db
+        print '\nBanco informa: {} desconectou-se'.format(self.__user[1])
+
+    def __logar(self):
+        """ Método para verificar a senha do usuário """
+        __senha = self.__user[2]
+        senha = raw_input('Digite a sua senha: ')
+        while senha != __senha:
+            print 'Senha incorreta!'
 
     def criar_usuario(self, nome, senha, agencia, conta):
         """ Método para  criar um usuário na tabela contas do banco.db """
@@ -75,24 +87,117 @@ class Client(object):
         else:
             return 0
 
-
     def consultar_saldo(self):
         """ Método para consultar o saldo do usuário """
-        print "Saldo do usuário: {}".format(self.__user[5])
+        print "Saldo do usuário: R${:.2f}\n".format(self.__user[5])
 
     def emitir_extrato(self):
         """ Emite o extrato do usuário se o mesmo quiser """
-        pass
+        try:
+            user_id = self.__user[0]
+            results = self.__cur.execute('''SELECT * FROM transacoes WHERE client_id={}'''.format(user_id)).fetchall()
+            if len(results) >= 1:
+                for r in results:
+                    print 'Há uma ação com valor de R${:.2f} envolvendo você\n'.format(r[3])
+            else:
+                print 'Você ainda não tem transações realizadas\n'
+        except Exception as e:
+            pass
     
-    def transferir(self):
+    def transferir(self, agencia, conta):
         """ Transferir dinheiro de uma conta para outra """
-        pass
+        try:
+            saldo_atual, user_id = self.__user[5], self.__user[0]
+            r = self.__cur.execute('''SELECT * FROM contas WHERE agencia = {} AND conta = {}'''.format(agencia, conta)).fetchall()[0]
+            saldo_atual_dest, id_dest = r[5], r[0]
+            valor = float(raw_input('Digite o valor a transferir: R$'))
+            tmp = raw_input('Deseja transferir R${:.2f} para {}? (S/N) '.format(valor, r[1])).upper()
+            if tmp == 'S':
+                if valor > saldo_atual:
+                    print 'Você não tem dinheiro suficiente para essa transferência'
+                else:
+                    saldo_atual -= valor
+                    saldo_atual_dest += valor
+                    print "Valor do usuário atual agora R${:.2f}, valor do outro usuário agora {:.2f}".format(saldo_atual, saldo_atual_dest)
+                    self.__cur.execute('''UPDATE contas SET saldo = {} where id = {}'''.format(saldo_atual, self.__user[0]))
+                    self.__cur.execute('''UPDATE contas SET saldo = {} WHERE id = {}'''.format(saldo_atual_dest, r[0]))
+                    self.__cur.execute('''INSERT INTO transacoes (client_id, destination_id, value, data) 
+                                                    VALUES(?, ?, ?, ?)''', (user_id, id_dest, valor, strftime('%d/%m/%y')))
+                    self.__conn.commit()
+                    self.__user = self.__checar_usuario()
+                    print 'Valor transferido com sucesso!\n'
+            elif tmp == 'N':
+                print 'OK! Não iremos transferir nenhuma quantia a esta conta'
+            else:
+                print 'Opção inválida'
+        except Exception as e:
+            raise Exception(e)
     
     def depositar(self):
         """ Depositar dinheiro na conta """
-        pass
+        try:
+            saldo_atual = self.__user[5]
+            valor = float(raw_input('Quanto você deseja depositar? R$'))
+            tmp = raw_input('Deseja depositar R${:.2f} nesta conta de {}? (S/N) '.format(valor, self.__user[1])).upper()
+            if tmp == 'S':
+                saldo_atual += valor
+                self.__cur.execute('''UPDATE contas SET saldo = {} WHERE id = {}'''.format(saldo_atual, self.__user[0]))
+                self.__cur.execute('''INSERT INTO transacoes (client_id, destination_id, value, data)
+                                                            VALUES(?, ?, ?, ?)''', (self.__user[0],
+                                                                                    self.__user[0],
+                                                                                    valor,
+                                                                                    strftime('%d/%m/%y')))
+                self.__conn.commit()
+                self.__user = self.__checar_usuario()
+                print 'Depósito feito com sucesso!\n'
+            elif tmp == 'N':
+                print 'OK! Não iremos depositar nenhuma quantia nesta conta'
+            else:
+                print 'Opção inválida'
+        except Exception as e:
+            raise Exception(e)
+
+    def depositar_em_outra(self, agencia, conta):
+        """ Depositar dinheiro em oura conta """
+        try:
+            r = self.__cur.execute('''SELECT * FROM contas WHERE agencia = {} AND conta = {}'''.format(agencia, conta)).fetchall()[0]
+            user_id, id_dest, saldo_atual = self.__user[0], r[0], r[5]
+            valor = float(raw_input('Valor do depósito: R$'))
+            tmp = raw_input('Deseja depositar R${:.2f} na conta de {}? (S/N) '.format(valor, r[1])).upper()
+            if tmp == 'S':
+                saldo_atual += valor
+                self.__cur.execute('''UPDATE contas SET saldo = {} WHERE id = {}'''.format(valor, id_dest))
+                self.__cur.execute('''INSERT INTO transacoes (client_id, destination_id, value, data)
+                                                        VALUES(?, ?, ?, ?)''', (user_id, id_dest, valor, strftime('%d/%m/%y')))
+                self.__conn.commit()
+                print 'Depósito feito com sucesso!\n'
+            elif tmp == 'N':
+                print 'OK! Não iremos depositar nenhuma quantia na conta'
+            else:
+                print 'Opção inválida'
+        except Exception as e:
+            raise Exception(e)
 
     def sacar(self):
         """ Sacar dinheiro da conta """
-        pass
-
+        try:
+            saldo_atual = self.__user[5]
+            valor = float(raw_input('Digite o valor que deseja sacar: R$'))
+            if valor > saldo_atual:
+                print 'Você não tem saldo suficiente para sacar'
+            else:
+                if valor > 1000:
+                    print 'Não é permitido sacar mais que R$1000'
+                else:
+                    try:
+                        notas = {100: 0, 50: 0, 20: 0}
+                        notas[100] = valor // 100
+                        resto = valor % 100
+                        notas[50] = resto // 50
+                        resto = resto % 50
+                        notas[20] = resto // 20
+                        print 'Você pode pegar {} nota(s) de 100, {} nota(s) de 50, {} nota(s) de 20\n'.format(notas[100], notas[50], notas[20])
+                    except Exception as e:
+                        raise Exception(e)
+        except Exception as e:
+            raise Exception(e)
--- exercises/buguinho_dev/main.py
+++ exercises/buguinho_dev/main.py
@@ -25,7 +25,33 @@ def menu():
             agencia = v.agencia(raw_input('Digite o número da agência: '))
             conta = v.conta(raw_input('Digite o número da conta: '))
             with Client(agencia, conta) as cliente:
-                cliente.consultar_saldo()
+                esc = 0
+                while esc != 6:
+                    try:
+                        esc = int(raw_input('[1] - Consultar saldo\n[2] - Extrato\n[3] - Transferir\n[4] - Depositar'
+                                        '\n[5] - Depositar em outra conta\n[6] - Saque\n[7] - Sair\n: '))
+                    except ValueError:
+                        print  'Informe uma opção válida\n'
+                    if esc == 1:
+                        cliente.consultar_saldo()
+                    elif esc == 2:
+                        cliente.emitir_extrato()
+                    elif esc == 3:
+                        d_agencia = v.agencia(raw_input('Digite o número da agência: '))
+                        d_conta = v.conta(raw_input('Digite o número da conta: '))
+                        cliente.transferir(d_agencia, d_conta)
+                    elif esc == 4:
+                        cliente.depositar()
+                    elif esc == 5:
+                        d_agencia = v.agencia(raw_input('Digite o número da agência: '))
+                        d_conta = v.conta(raw_input('Digite o número da conta: '))
+                        cliente.depositar_em_outra(d_agencia, d_conta)
+                    elif esc == 6:
+                        cliente.sacar()
+                    elif esc == 7:
+                        exit(0)
+                    else:
+                        print 'Opção inválida'
         elif tmp == 2:
             nome = v.nome(raw_input('Digite o seu nome completo: '))
             senha = v.senha(raw_input('Digite a senha: '))
@@ -34,7 +60,7 @@ def menu():
             with Client() as cliente:
                 print cliente.criar_usuario(nome, senha, agencia, conta)
         elif tmp == 3:
-            exit(1)
+            exit(0)
 
 if __name__ == '__main__':
     menu()

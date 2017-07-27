3b8df8c57e2ac7c0fa91b133dc9ebd340ae0cbab
Reni A. Dantas <reni.alkimim@gmail.com>
2017-07-27 12:44:09 -0300

    Another exercise

---
+++ exercises/guanabara/ex19.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+""" 
+    Exercícios proposto por: https://www.youtube.com/channel/UCrWvhVmt0Qac3HgsjQK62FQ
+    Canal Curso Em Vídeo
+    Profº Gustavo Guanabara
+"""
+import random
+
+lst = [1,5,3,2,9]
+print(random.choice(lst))

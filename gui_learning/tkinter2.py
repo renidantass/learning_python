c0d0cd72b370e2bf37e404ddf916d2ea30cd763f
Reni Dantas <reni.alkimim@gmail.com>
2017-12-19 22:56:55 -0200

    Splash screen on gui learning with tkinter, WTF

---
+++ gui_learning/tkinter2.py
@@ -0,0 +1,11 @@
+"""
+    How to create a splash screen to your software :D
+"""
+from tkinter import *
+
+
+class SplashScreen(Tk)
+    def __init__(self):
+        self.attributes('-type', 'splash')
+        self.geometry('200x200+0+0')
+        self.mainloop()
